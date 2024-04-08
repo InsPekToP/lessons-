3dc5b2d68bf905be3e46b520cf985fbc1bc1543c
Mikl <inspektop1@gmail.com>
2024-04-08 14:05:53 +0200

    Create Ship.py

---
+++ 2_module/classes/Ship.py
@@ -0,0 +1,8 @@
+from classes.Transport import Transport as Tr #из транспорта импортировали транспорт и назвали Tr
+
+class Ship(Tr):
+    pass  
+
+# bmw = Transport(200,2000,True) #создаем обьект на основе класса Transport и передаем ему 3 пар-ра
+# train = Transport(140,20000,False)
+# ship = Transport(50,5000,True)
