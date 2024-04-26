b109d67343127cd4819add1f42b44a8b7906282a
Mikl <inspektop1@gmail.com>
2024-04-26 11:47:04 +0200

    Create main2_1s.py

---
+++ 1_module/main2_1s.py
@@ -0,0 +1,39 @@
+#main2_1s.py
+#Переменные и получение данных от пользователя
+info = 5
+print(info)
+info = 70
+print(info)
+print("Result:",info,"!")
+
+#del info
+a1 = 10 #Integer(int)
+a2 = -5.56789 #Float
+a3 = '5.6' #String(str)
+a4 = False #Boolean
+
+print(str(a1) + a3)
+print(bool(a1))
+
+a1 = "ten"
+print(a1)
+
+
+
+x,y = 5,True
+a = b = 10
+
+num1 = 5
+num2 = 10.5
+
+num1 -= 5.5
+#num1++
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+
+
+
+
