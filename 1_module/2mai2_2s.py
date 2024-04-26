b25c67d03915563aee6e8a62f1321a2651477656
Mikl <inspektop1@gmail.com>
2024-04-26 11:46:03 +0200

    Create 2mai2_2s.py

---
+++ 1_module/2mai2_2s.py
@@ -0,0 +1,19 @@
+#2mai2_2s.py
+#Переменные и получение данных от пользователя
+# user = input("Enter num1: ")
+
+num1 = int(input("Enter num1: ")) #String
+num2 = int(input("Enter num2: ")) #String
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** num2)
+print(num1 // num2)
+
+# Самостоятельная работа
+# Используя функцию, запросите у пользователя ввод имени пользователя.
+# Выведите приветственное сообщение, используя введенное имя
+
+name = input("Введите ваше имя: ")
+print("Привет:",name)
