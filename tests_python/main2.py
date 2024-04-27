be3bc59b1da8a8047b9379fc5b7a5c7a0b877d6d
Mikl <inspektop1@gmail.com>
2024-04-27 22:15:37 +0200

    Create main2.py

---
+++ tests_python/main2.py
@@ -0,0 +1,24 @@
+#main2.py
+number = 5
+
+print("Результат: ", number)
+
+del number
+
+number = 7
+print("Результат: ",number)
+
+number = 9 #int
+print("Результат: ",number)
+digit = 4.5565854 #float
+word = "Результат:" #string
+boolean = True #bool
+#boolean = False
+
+str_num = "5" #string
+
+#print(word + str(digit))
+
+print(word + str(number + int(str_num)))
+
+print(word,boolean)
