40723d32956cd78e754ba0309a1dbf6a17bb753f
Mikl <inspektop1@gmail.com>
2024-04-27 22:14:40 +0200

    Create main.py

---
+++ tests_python/main.py
@@ -0,0 +1,19 @@
+#main.py
+#Comment
+#print("Hello itProger")
+#print(5)
+#print("Результат: 5")
+#print("Результат: ",7,10,".")
+print("Результат: ",8,15,"Second line ",sep=".", end="!\n")#как убрать точку в начале непонятно!!!
+#print("Second line")
+#print('Second \" \t \\l\ni\nn\ne')
+#print(5*5)
+#print("Результат:",5**3)
+#print("Результат:",5//3)
+#print("Результат:",min(9,0,2,5,-8))
+#print("Результат:",max(9,0,2,5,-8))
+#print("Результат:",abs(-5))
+#print("Результат:",pow(5,2))
+#print("Результат:",round(5/3))
+
+#input("Введите свой возраст: ")
