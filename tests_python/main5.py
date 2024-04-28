4346c7d2d7b3fcbdd64984e57e36f836bc76828f
Mikl <inspektop1@gmail.com>
2024-04-28 21:11:07 +0200

    Create main5.py

---
+++ tests_python/main5.py
@@ -0,0 +1,12 @@
+#main5.py
+# data = input()
+#
+# if data == "Five":
+#     number = 5
+# else:
+#     number = 0
+# print(number)
+
+data = input()
+number = 5 if data == "Five" else 0
+print(number)
