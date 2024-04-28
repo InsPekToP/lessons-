b2d6d0b11758f305349d79d91e6cd5b221a6d80a
Mikl <inspektop1@gmail.com>
2024-04-28 21:10:05 +0200

    Create main4.py

---
+++ tests_python/main4.py
@@ -0,0 +1,41 @@
+#main4.py
+#if 5 == 5:
+    #print("Yes")
+    #print("!!!")
+
+#if 5 > 5:
+    #print("Yes")
+    #print("!!!")
+
+#user_data = int(input("Введите число: "))
+
+#if user_data != 5:
+    #print("Мы на месте")
+    #if user_data >6:
+        #print("Number is bigger than 5")
+        #print(" re")
+
+user_data = int(input("Введите число: "))
+
+isHappy = False
+
+if isHappy or user_data==6:
+    print("User is happy")
+elif user_data == 5:
+    print("Number is 5")
+elif user_data == 7:
+    print("Number is 7")
+else:
+    print("User is unhappy")
+
+#if user_data != 5:
+#    print("Мы на месте")
+#    if user_data >6:
+#        print("Number is bigger than 5")
+#        print(" re")
+
+#isHappy = False
+
+#if not isHappy:
+#    print("User is happy")
+    
