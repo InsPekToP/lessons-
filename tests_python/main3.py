4c84a9696724de29198408c77c1c34bbd8f81a2d
Mikl <inspektop1@gmail.com>
2024-04-28 21:09:18 +0200

    Create main3.py

---
+++ tests_python/main3.py
@@ -0,0 +1,31 @@
+#main3.py
+num1 = int(input("Введите первое число: "))
+#num1 = input("Введите первое число: ")
+#string -текст
+#num1 = int(input("Введите первое число: "))
+#int - число
+num2 = int(input("Введите второе чило:"))
+
+#num1 = num1 + 5  но лучше более сокращенный---
+num1 += 5
+#num1 -= 5
+#num1 *= 5
+#num1 /= 5
+#num1 %= 5
+
+
+print("Result: ",num1 + num2)
+print("Result: ",num1 - num2)
+print("Result: ",num1 / num2)
+print("Result: ",num1 * num2)
+print("Result: ",num1 ** num2)
+print("Result: ",num1 // num2)
+
+word = "Hi" #string
+print(word*2)
+
+word = "hello"
+print(word*2)
+
+#del
+#word = 45.5
