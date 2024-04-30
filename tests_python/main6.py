b67565db9a716022ac4a3c589209f7bd2c0322d8
Mikl <inspektop1@gmail.com>
2024-04-30 15:54:15 +0200

    Create main6.py

---
+++ tests_python/main6.py
@@ -0,0 +1,29 @@
+#main6.py
+#6 Циклы,операторы циклов
+for i in range(6) :# от нуля до 6 не включительно(как сделать в линию пока не понятно)--прописывать end= в print
+    print(i, end = "\\")
+#
+# for i in range(1,6): #от 1 до 6 не влючительно
+#     print(i)
+#
+# for i in range(1,6,2): #с шагом 2
+#     print(i)
+
+
+# word = "Hello World"
+# for i in word:
+#     print(i*3,end = "")
+
+
+# word = "Hello World"
+# for i in word:
+#     if i == "!":
+#         print("Yes")
+
+
+# count = 0
+# word = "Hello World"
+# for i in word:
+#     if i == "W":
+#         count +=1
+# print("Count:",count)
