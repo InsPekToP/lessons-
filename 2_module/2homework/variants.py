7a7f2c6c5830131e22df0fc9dfc7bfd377e6dc6c
Mikl <inspektop1@gmail.com>
2024-05-05 13:24:58 +0200

    Create variants.py

---
+++ 2_module/2homework/variants.py
@@ -0,0 +1,14 @@
+#variants.py
+#2main_homework.py
+from enum import Enum
+
+# class Variants(Enum):
+#     SCISSORS = 'scissors'
+#     ROCK = 'rock'
+#     PAPER = 'paper'
+
+#Правильный вариант:
+class Variants(Enum):
+    SCISSORS = 0,
+    ROCK = 1,
+    PAPER = 2
