f075f5a9d8dd79b9d8c60fdf23e81b7dd5e22fbe
Mikl <inspektop1@gmail.com>
2024-05-15 21:39:41 +0200

    Create main14_#8.py

---
+++ tests_python/main14_#8.py
@@ -0,0 +1,16 @@
+#main14_#8.py
+#срезы
+word = 'Football'
+# print(word[0:4])
+# print(word[4:])
+# print(word[4:-1])
+# print(word[1:-1:1])
+
+lis = [6,4,"Stroka",True,6.5]
+# print(lis[2])
+print(lis[2:5:2])
+print(lis[::])
+print(lis[:4:])
+print(lis[::-1])
+print(lis[::-2])
+print(lis[::2])
